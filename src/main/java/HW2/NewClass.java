f8f9616101dd4459e5eae92e896abcbe77a426a2
suleman <sulem@MeowbookAir.home>
2021-09-20 21:11:23 -0400

    Method 1

--- src/main/java/HW2/NewClass.java
+++ src/main/java/HW2/NewClass.java
@@ -10,15 +10,53 @@ import java.util.concurrent.TimeUnit;
 import java.time.*;
 import java.util.Scanner;
 public class NewClass {
+    
+    int[] arr;
+    int one;
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int getOne() {
+        return one;
+    }
+
+    public void setOne(int one) {
+        this.one = one;
+    }
+    
+    public int maxPair(NewClass theClass){
+        arr = theClass.getArr();
+        one = theClass.getOne();
+        int max = arr[0];
+        for(int i = 0 ; i < arr.length ; i++)
+            if(arr[i] > max)
+                max = arr[0];
+        return max + one;
+    }
+    
+    public NewClass(int theInt , int[] theArr){
+        one = theInt;
+        arr = theArr;
+    }
+    
+    
  public static void main(String[] args) throws ParseException {
  Instant start = Instant.now();
 
  long i=0;
- while (i<100000000){
- i++;
+ while (i<100000000){i++;}
 
- }
-
+ 
+ 
+ 
+ 
+ 
  Instant finish = Instant.now();
  long timeElapsed = Duration.between(start, finish).toMillis(); //in millis
  System.out.println("time "+ timeElapsed);
